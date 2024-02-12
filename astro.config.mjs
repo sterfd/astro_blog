4f3b31703f70662430c0453b40780a0402a09b47
sterf-mac <stefhwin@gmail.com>
2024-02-12 11:12:12 -0500

    light and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nkmt.netflify.app"
+  site: "https://nkmt.netflify.app",
+  integrations: [preact()]
 });
